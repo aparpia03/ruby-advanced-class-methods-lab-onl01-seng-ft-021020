52768120d6dfe18d521ec73163fbeb68b20b4b35
Abbas Parpia <aparpia03@gmail.com>
2020-02-21 20:26:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,19 +12,19 @@ class Song
   end
 
 def self.create
-  song = Song.new 
+  song = self.new 
   @@all << song
   song 
 end 
 
 def self.new_by_name(name)
-  song = Song.new 
+  song = self.new 
   song.name = name 
   song 
   end 
   
 def self.create_by_name(name)
-  song = Song.new 
+  song = self.new 
   
 end   
 
