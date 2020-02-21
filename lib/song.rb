cd5e927b632bb3e43fbe585ed9ee42ead77e0aac
Abbas Parpia <aparpia03@gmail.com>
2020-02-21 20:30:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,26 +11,5 @@ class Song
     self.class.all << self
   end
 
-def self.create
-  song = self.new 
-  @@all << song
-  song 
-end 
-
-def self.new_by_name(name)
-  song = self.new 
-  song.name = name 
-  song 
-  end 
-  
-def self.create_by_name(name)
-    song = self.new 
-    song.name = title
-    song
-end 
-
-def self.find_by_name(song)
-  
-
 
 end 
